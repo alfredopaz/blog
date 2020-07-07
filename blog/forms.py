e0fcc82f6ac1ca1889182fa36bc4e9b4b5380249
Alfredo Paz <alfredopaz7@gmail.com>
2020-07-07 15:26:48 -0500

    Se creo un ModelForm para el modelo Artículo

---
+++ blog/forms.py
@@ -0,0 +1,13 @@
+from django import forms
+
+from .models import Blog
+
+class BlogForm(forms.ModelForm):
+  class Meta:
+    model = Blog
+    fields = [
+        'title',
+        'content',
+        'active'
+        ]
+
